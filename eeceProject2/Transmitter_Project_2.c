77fe1c501eeab478c967922391353cb17c29f78a
tfulton5 <travis_f25@hotmail.com>
2014-03-18 19:32:07 -0700

    Transmitter skeleton code

---
+++ eeceProject2/Transmitter_Project_2.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <at89lp51rd2.h>
+
+#define CLK 22118400L
+#define BAUD 115200L
+#define BRG_VAL (0x100-(CLK/(32L*BAUD)))
+
+//We want timer 0 to interrupt every 100 microseconds ((1/10000Hz)=100 us)
+#define FREQ 10000L
+#define TIMER0_RELOAD_VALUE (65536L-(CLK/(12L*FREQ)))
+#define TIMER1_RELOAD_VALUE (65536L-(CLK/(12L*FREQ)))
+
+unsigned char _c51_external_startup(void)
+{
+	// Configure ports as a bidirectional with internal pull-ups.
+	P0M0=0;	P0M1=0;
+	P1M0=0;	P1M1=0;
+	P2M0=0;	P2M1=0;
+	P3M0=0;	P3M1=0;
+	AUXR=0B_0001_0001; // 1152 bytes of internal XDATA, P4.4 is a general purpose I/O
+	P4M0=0;	P4M1=0;
+    
+    // Initialize the serial port and baud rate generator
+    PCON|=0x80;
+	SCON = 0x52;
+    BDRCON=0;
+    BRL=BRG_VAL;
+    BDRCON=BRR|TBCK|RBCK|SPD;
+	
+	// Initialize timer 0 for ISR 'pwmcounter()' below
+	TR0=0; // Stop timer 0
+	TMOD=0x01; // 16-bit timer
+	// Use the autoreload feature available in the AT89LP51RB2
+	// WARNING: There was an error in at89lp51rd2.h that prevents the
+	// autoreload feature to work.  Please download a newer at89lp51rd2.h
+	// file and copy it to the crosside\call51\include folder.
+	TH0=RH0=TIMER0_RELOAD_VALUE/0x100;
+	TL0=RL0=TIMER0_RELOAD_VALUE%0x100;
+	TR0=1; // Start timer 0 (bit 4 in TCON)
+	ET0=1; // Enable timer 0 interrupt
+	EA=1;  // Enable global interrupts
+	
+	pwmcount=0;
+    
+    return 0;
+}
+
+void tx_byte ( unsigned char val )
+{
+	unsigned char j;
+	//Send the start bit
+	txon=0;
+	wait_bit_time();
+	for (j=0; j<8; j++)
+	{
+		txon=val&(0x01<<j)?1:0;
+		wait_bit_time();
+	}
+	txon=1;
+	//Send the stop bits
+	wait_bit_time();
+	wait_bit_time();
+}
+void SPIWrite(unsigned char value)
+{
+	SPSTA&=(~SPIF); // Clear the SPIF flag in SPSTA
+	SPDAT=value;
+	while((SPSTA & SPIF)!=SPIF); //Wait for transmission to end
+}
+
+// Read 10 bits from the MCP3004 ADC converter
+unsigned int GetADC(unsigned char channel)
+{
+	unsigned int adc;
+
+	// initialize the SPI port to read the MCP3004 ADC attached to it.
+	SPCON&=(~SPEN); // Disable SPI
+	SPCON=MSTR|CPOL|CPHA|SPR1|SPR0|SSDIS;
+	SPCON|=SPEN; // Enable SPI
+	
+	P1_4=0; // Activate the MCP3004 ADC.
+	SPIWrite(channel|0x18);	// Send start bit, single/diff* bit, D2, D1, and D0 bits.
+	for(adc=0; adc<10; adc++); // Wait for S/H to setup
+	SPIWrite(0x55); // Read bits 9 down to 4
+	adc=((SPDAT&0x3f)*0x100);
+	SPIWrite(0x55);// Read bits 3 down to 0
+	P1_4=1; // Deactivate the MCP3004 ADC.
+	adc+=(SPDAT&0xf0); // SPDR contains the low part of the result. 
+	adc>>=4;
+		
+	return adc;
+}
+
+float voltage (unsigned char channel)
+{
+	return ( (GetADC(channel)*4.84)/1023.0 ); // VCC=4.84V (measured)
+}
+
+void main (void)
+{
+	while(1) {
+
+	}
+}
