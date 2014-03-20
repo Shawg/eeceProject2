4d01fae99c36608d9895e72f724b1e7d00214da9
tfulton5 <travis_f25@gmail.com>
2014-03-20 16:18:07 -0700

    Finished everything but circuit related things

--- eeceProject2/Transmitter_Project_2.c
+++ eeceProject2/Transmitter_Project_2.c
@@ -1,15 +1,6 @@
 #include <stdio.h>
 #include <at89lp51rd2.h>
 
-#define CLK 22118400L
-#define BAUD 115200L
-#define BRG_VAL (0x100-(CLK/(32L*BAUD)))
-
-//We want timer 0 to interrupt every 100 microseconds ((1/10000Hz)=100 us)
-#define FREQ 10000L
-#define TIMER0_RELOAD_VALUE (65536L-(CLK/(12L*FREQ)))
-#define TIMER1_RELOAD_VALUE (65536L-(CLK/(12L*FREQ)))
-
 unsigned char _c51_external_startup(void)
 {
 	// Configure ports as a bidirectional with internal pull-ups.
@@ -19,43 +10,21 @@ unsigned char _c51_external_startup(void)
 	P3M0=0;	P3M1=0;
 	AUXR=0B_0001_0001; // 1152 bytes of internal XDATA, P4.4 is a general purpose I/O
 	P4M0=0;	P4M1=0;
-    
-    // Initialize the serial port and baud rate generator
-    PCON|=0x80;
-	SCON = 0x52;
-    BDRCON=0;
-    BRL=BRG_VAL;
-    BDRCON=BRR|TBCK|RBCK|SPD;
-/*	
-	// Initialize timer 0 for ISR 'pwmcounter()' below
-	TR0=0; // Stop timer 0
-	TMOD=0x01; // 16-bit timer
-	// Use the autoreload feature available in the AT89LP51RB2
-	// WARNING: There was an error in at89lp51rd2.h that prevents the
-	// autoreload feature to work.  Please download a newer at89lp51rd2.h
-	// file and copy it to the crosside\call51\include folder.
-	TH0=RH0=TIMER0_RELOAD_VALUE/0x100;
-	TL0=RL0=TIMER0_RELOAD_VALUE%0x100;
-	TR0=1; // Start timer 0 (bit 4 in TCON)
-	ET0=1; // Enable timer 0 interrupt
-	EA=1;  // Enable global interrupts
-	
-	pwmcount=0;
-*/
+
     return 0;
 }
-//This waits 
+//This currently waits 500ms 
 void wait_bit_time(void)
 {
 	_asm	
 		;For a 22.1184MHz crystal one machine cycle 
 		;takes 12/22.1184MHz=0.5425347us
-	    mov R2, #20
+	    mov R2, #10
 	L3:	mov R1, #248
 	L2:	mov R0, #184
 	L1:	djnz R0, L1 ; 2 machine cycles-> 2*0.5425347us*184=200us
 	    djnz R1, L2 ; 200us*250=0.05s
-	    djnz R2, L3 ; 0.05s*20=1s
+	    djnz R2, L3 ; 0.05s*10=500ms
 	    ret
     _endasm;
 }
@@ -64,22 +33,49 @@ void tx_byte(unsigned char val)
 {
 	unsigned char j;
 	//Send the start bit
-	txon=0;
+	//txon=0; make the circuit turn off
 	wait_bit_time();
 	for (j=0; j<8; j++)
 	{
-		txon=val&(0x01<<j)?1:0;
+		//txon=val&(0x01<<j)?1:0; make tx control the circuit
 		wait_bit_time();
 	}
-	txon=1;
+	//txon=1; make the ciruit turn on
 	//Send the stop bits
 	wait_bit_time();
 	wait_bit_time();
 }
 
+void moveCloser(void) 
+{
+	tx_byte(0xfd); // move closer is 11111101
+}
+
+void moveFarther(void)
+{
+	tx_byte(0xf5); // move farther is 11110101
+}
+
+void rotate180(void)
+{
+	tx_byte(0xd5); // rotate is 11010101
+}
+
+void prlPark(void)
+{
+	tx_byte(0x55); // parallel park is 01010101
+}
+
 void main (void)
 {
 	while(1) {
-
+		// if button 1, moveCloser
+		if(P2_2 != 0) moveCloser();
+		// if button 2, moveFarther
+		if(P2_3 != 0) moveFarther();
+		// if button 3, rotate 180
+		if(P4_3 != 0) rotate180();
+		// if button 4, parallel park
+		if(P3_7 != 1) prlPark();
 	}
 }
