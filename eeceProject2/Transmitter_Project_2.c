8d038aaf551bec6088d85de1fb8151279971988a
tfulton5 <travisf25@gmail.com>
2014-03-31 14:21:42 -0700

    put H-bridge code into transmitter

--- eeceProject2/Transmitter_Project_2.c
+++ eeceProject2/Transmitter_Project_2.c
@@ -6,17 +6,49 @@
 #define ROTATE_180 0xd5
 #define PRL_PARK 0x55
 
+#define FREQ (15105.6463263*2.0)
+#define TIMER0_RELOAD_VALUE (long int) (65536.0-(CLK/(12.0*FREQ)))
+
 unsigned char _c51_external_startup(void)
 {
 	// Configure ports as a bidirectional with internal pull-ups.
 	P0M0=0;	P0M1=0;
-	P1M0=0;	P1M1=0;
+	P1M0=0;	P1M1=0x07; //push-pull
 	P2M0=0;	P2M1=0;
 	P3M0=0;	P3M1=0;
 	AUXR=0B_0001_0001; // 1152 bytes of internal XDATA, P4.4 is a general purpose I/O
 	P4M0=0;	P4M1=0;
 
+	 // Initialize the serial port and baud rate generator
+    PCON|=0x80;
+	SCON = 0x52;
+    BDRCON=0;
+    BRL=BRG_VAL;
+    BDRCON=BRR|TBCK|RBCK|SPD;
+	
+	// Initialize timer 0 for ISR 'pwmcounter()' below
+	TR0=0; // Stop timer 0
+	TMOD=0x01; // 16-bit timer
+	// Use the autoreload feature available in the AT89LP51RB2
+	// WARNING: There was an error in at89lp51rd2.h that prevents the
+	// autoreload feature to work.  Please download a newer at89lp51rd2.h
+	// file and copy it to the crosside\call51\include folder.
+	TH0=RH0=TIMER0_RELOAD_VALUE/0x100;
+	TL0=RL0=TIMER0_RELOAD_VALUE%0x100;
+	TR0=1; // Start timer 0 (bit 4 in TCON)
+	ET0=1; // Enable timer 0 interrupt
+	EA=1;  // Enable global interrupts
+	
+	P1_0=0;
+	P1_1=1;
+
     return 0;
+}
+
+void pwmcounter (void) interrupt 1
+{
+	P1_0=!P1_0;
+	P1_1=!P1_1;
 }
 // This currently waits 100ms. If you change this function, change it in
 // reciever program as well. Also change the 1.5 bit time function
@@ -76,12 +108,12 @@ void main (void)
 {
 	while(1) {
 		// if button 1, moveCloser
-		if(P2_2 != 0) moveCloser();
+		//if(P2_2 != 0) moveCloser();
 		// if button 2, moveFarther
-		if(P2_3 != 0) moveFarther();
+		//if(P2_3 != 0) moveFarther();
 		// if button 3, rotate 180
-		if(P4_3 != 0) rotate180();
+		//if(P4_3 != 0) rotate180();
 		// if button 4, parallel park
-		if(P3_7 != 1) prlPark();
+		//if(P3_7 != 0) prlPark();
 	}
 }
